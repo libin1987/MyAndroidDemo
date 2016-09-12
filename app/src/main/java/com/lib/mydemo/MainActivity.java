aa5c6224829732b7e7cf978cbfec5cac473a6b88
libin1987 <lib1987@qq.com>
2016-09-12 18:32:48 +0800

    新更新

--- app/src/main/java/com/lib/mydemo/MainActivity.java
+++ app/src/main/java/com/lib/mydemo/MainActivity.java
@@ -1,47 +1,18 @@
 package com.lib.mydemo;
 
 import android.os.Bundle;
-import android.view.View;
 
-import com.handmark.pulltorefresh.library.PullToRefreshListView;
 import com.lib.base.BaseActivity;
-import com.lib.base.RefreshListHelper;
-import com.lib.mydemo.adapter.ListItemAdapter;
-import com.lib.mydemo.model.ListItemModel;
-import com.lib.utility.CustomProgressDialog;
-
-import org.xutils.view.annotation.Event;
-import org.xutils.view.annotation.ViewInject;
-
-import java.util.ArrayList;
 
 
 public class MainActivity extends BaseActivity {
 
-    @ViewInject(R.id.pullToRefreshListView)
-    PullToRefreshListView lv;
-    RefreshListHelper<ListItemModel> refreshListHelper=new RefreshListHelper<>(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<ListItemModel> datas = new ArrayList();
-        datas.add(new ListItemModel("name1", "content"));
-        datas.add(new ListItemModel("name2", "content"));
-        datas.add(new ListItemModel("name3", "content"));
-
-        refreshListHelper.pullToRefreshListView = lv;
-        refreshListHelper.adapter = new ListItemAdapter();
-        refreshListHelper.tempData = datas;
-        refreshListHelper.api = "";
-        refreshListHelper.initPullToRefreshListView();
     }
 
-
-    @Event(R.id.toolbar_title)
-    private void onClick(View view) {
-        CustomProgressDialog.showsys(this);
-    }
 }
---
+++ app/src/main/java/com/lib/mydemo/PullToRefreshListView.java
@@ -0,0 +1,36 @@
+package com.lib.mydemo;
+
+import android.os.Bundle;
+
+import com.lib.base.BaseActivity;
+import com.lib.base.RefreshListHelper;
+import com.lib.mydemo.adapter.ListItemAdapter;
+import com.lib.mydemo.model.ListItemModel;
+
+import org.xutils.view.annotation.ViewInject;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PullToRefreshListView extends BaseActivity {
+    @ViewInject(R.id.pullToRefreshListView)
+    com.handmark.pulltorefresh.library.PullToRefreshListView lv;
+    RefreshListHelper<ListItemModel> refreshListHelper=new RefreshListHelper<>(this);
+
+    List<ListItemModel> datas = Arrays.asList(
+            new ListItemModel("name1", "content"),
+            new ListItemModel("name2", "content"),
+            new ListItemModel("name3", "content")
+    );
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pull_to_refresh_list_view);
+
+        refreshListHelper.pullToRefreshListView = lv;
+        refreshListHelper.adapter = new ListItemAdapter();
+        refreshListHelper.tempData = datas;
+        refreshListHelper.api = "";
+        refreshListHelper.initPullToRefreshListView();
+    }
+}
